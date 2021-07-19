9a94cf8aae8a5e1d02f9f2cb5e0358c9d9b1336f
Yashprime1 <63970982+Yashprime1@users.noreply.github.com>
2021-07-20 00:27:26 +0530

    Update settings.py

--- capgemini/settings.py
+++ capgemini/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import django_heroku
 import os
 from pathlib import Path
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
